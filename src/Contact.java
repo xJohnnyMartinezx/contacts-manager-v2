f1752c8e28ef6e8814c9ecb82097bfa3ec2ad572
Johnny Martinez <mrtnz.jn@gmail.com>
2023-05-01 16:50:39 -0500

    created Contact and Menu classes

---
+++ src/Contact.java
@@ -0,0 +1,43 @@
+public class Contact {
+
+//    ***** PROPERTIES
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+//    ***** CONSTRUCTOR
+
+    public Contact(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+//    ***** GETTERS AND SETTERS
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
